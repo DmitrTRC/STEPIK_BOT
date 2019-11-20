dd6e1e0f68a54b8271bee6cdecbf04407e7be622
Dmitry Morozov <9984398@gmail.com>
2019-11-20 19:33:17 +0100

    First replies added

--- Bot_Training_1.py
+++ Bot_Training_1.py
@@ -1,5 +1,6 @@
 import telebot
 import random
+from pprint import pprint
 
 token = '1035021917:AAF-3Fki6dWOpQyyTqBbxoFQAEAwYz6fTlk'
 
@@ -9,12 +10,16 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(func=lambda message: True)
 def echo(message):
     if message.text == '/start':
-        bot.reply_to(message,
-                     'Это БОТ пока он мало ,что умеет но активно учится :) В основном он синоптик! но поди знай ... ')
+        bot.send_message(message.chat.id,
+                         'Это БОТ пока он мало ,что умеет но активно учится :) В основном он синоптик! но поди знай ... '
+                         )
     elif message.text in ('привет', 'Hi', 'hi', 'Hi!', 'Привет!', 'привет!'):
-        bot.reply_to(message, 'Привет, привет')
+        bot.reply_to(message, 'Привет, привет ' + message.from_user.first_name)
     elif message.text == 'Погода на сегодня':
         bot.reply_to(message, 'Сегодня - сам в окно посмотри, не написал еще ...')
+
+    else:
+        bot.reply_to(message, 'Пока не понимаю , блин ...')
 
 
 bot.polling()
