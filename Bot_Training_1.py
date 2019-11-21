e5d6b02cf0cfc018e865c52f40028961f0cfad27
Dmitry Morozov <9984398@gmail.com>
2019-11-21 15:31:43 +0100

    Preparing for wiki

--- Bot_Training_1.py
+++ Bot_Training_1.py
@@ -1,4 +1,6 @@
 import telebot
+import requests
+import pywikibot
 import random
 from pprint import pprint
 
@@ -7,7 +9,27 @@ token = '1035021917:AAF-3Fki6dWOpQyyTqBbxoFQAEAwYz6fTlk'
 bot = telebot.TeleBot(token)
 
 
+def weather_request(city='None'):
+    url = 'http://wttr.in/'
+
+    if city == 'moon':
+        url = url + 'moon' + '?QFT&lang=ru'
+
+    elif city:
+
+        url = url + city + '?format=2'
+    else:
+        url = url + '?format=2'
+
+    ans = requests.get(url)
+    return ans.text
+
+
 @bot.message_handler(func=lambda message: True)
+def wiki( message):
+
+
+
 def echo(message):
     if message.text == '/start':
         bot.send_message(message.chat.id,
@@ -15,12 +37,17 @@ def echo(message):
                          )
     elif message.text in ('привет', 'Hi', 'hi', 'Hi!', 'Привет!', 'привет!'):
         bot.reply_to(message, 'Привет, привет ' + message.from_user.first_name)
-    elif message.text == 'Погода на сегодня':
-        bot.reply_to(message, 'Сегодня - сам в окно посмотри, не написал еще ...')
-    elif message.text == 'Москва':
-        bot.reply_to(message, 'Сейчас отличная погода!')
-    elif message.text == 'Москва завтра':
-        bot.reply_to(message, 'Завтра еще лучше!')
+    elif message.text.lower() in ('погода на сегодня', 'погода', 'какая погода', 'weather',
+                                  'what the weather', 'скажи погоду'):
+        bot.send_message(message.chat.id, message.from_user.first_name + ' , а погода сейчас : ' + weather_request())
+    elif message.text.lower() == 'москва':
+        bot.reply_to(message, 'Сейчас отличная погода! ' + weather_request('Москва'))
+    elif message.text.lower() in ('луна', 'moon', 'покажи луну', 'show moon'):
+        bot.reply_to(message, ' Все о луне что я знаю! ' + weather_request('moon'))
+    elif message.text.lower() in ('погода завтра', 'погода на завтра', 'weather for tomorrow'):
+        bot.reply_to(message, 'Завтра , будет: еще лучше !( Under construction)')
+    elif message.text.lower().split()[0] == 'wiki':
+        print('Wiki Detected')
 
     else:
         bot.reply_to(message, 'Я тебя не понял')
