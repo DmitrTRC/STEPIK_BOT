b75f218773e1b9dc4f4874f79137c874f3cfb14f
Dmitry Morozov <9984398@gmail.com>
2019-11-25 14:33:45 +0100

    Начало 3 недели. Wiki _is _working

--- Bot_Training_1.py
+++ Bot_Training_1.py
@@ -1,12 +1,11 @@
 import telebot
 import requests
-import pywikibot
+
 import random
 from pprint import pprint
+import wikipedia as wiki
 
 token = '1035021917:AAF-3Fki6dWOpQyyTqBbxoFQAEAwYz6fTlk'
-
-site = pywikibot.Site('en', 'wikipedia')
 
 bot = telebot.TeleBot(token)
 
@@ -27,14 +26,11 @@ def weather_request(city='None'):
     return ans.text
 
 
-def get_wiki(message, site):
-    print(f'Message before request wiki: {message=} ')
-    wiki_request = 'Wikipedia:' + message.replace('wiki', '')
-    print(wiki_request)
-    page = pywikibot.Page(site, wiki_request)
-    text = page.text
-    print(f'Answered {text=}')
-    return text
+def get_wiki(phrase):
+    try:
+        return wiki.summary(phrase)
+    except:
+        return " Request not found"
 
 
 @bot.message_handler(func=lambda message: True)
@@ -43,7 +39,7 @@ def echo(message):
         bot.send_message(message.chat.id,
                          'Это бот-погода. Поможет узнать погоду в любом городе. Какой город интересует?'
                          )
-    elif message.text in ('привет', 'Hi', 'hi', 'Hi!', 'Привет!', 'привет!'):
+    elif message.text.lower() in ('привет', 'hi', 'Hi!', 'привет!'):
         bot.reply_to(message, 'Привет, привет ' + message.from_user.first_name)
     elif message.text.lower() in ('погода на сегодня', 'погода', 'какая погода', 'weather',
                                   'what the weather', 'скажи погоду'):
@@ -51,7 +47,6 @@ def echo(message):
     elif message.text.lower() == 'москва':
         bot.reply_to(message, 'Сейчас отличная погода! ' + weather_request('Москва'))
     elif message.text.lower().find('город') == 0:
-        print('City weather detected')
         weather_message = message.text.lower().replace('город', '')
         print(weather_message.strip())
         bot.send_message(message.chat.id,
@@ -63,11 +58,9 @@ def echo(message):
         bot.reply_to(message, ' Все о луне что я знаю! ' + weather_request('moon'))
     elif message.text.lower() in ('погода завтра', 'погода на завтра', 'weather for tomorrow'):
         bot.reply_to(message, 'Завтра , будет: еще лучше !( Under construction)')
-    elif message.text.lower().split()[0] == 'wiki':
-
-        ans = get_wiki(message.text.lower(), site)
+    elif message.text.lower().startswith('wiki '):
+        ans = get_wiki(message.text[5:])
         bot.send_message(message.chat.id, ans)
-
 
     else:
         bot.send_message(message.chat.id, 'Я тебя не понял')
