03405ee4040ab749ecca7e4194edde575e0ae682
Dmitr Morozov <50495488+DmitrTRC@users.noreply.github.com>
2019-11-27 08:55:51 +0100

    Delete Weather_req.py
    Do not need

--- Weather_req.py
+++
@@ -1,8 +0,0 @@
-import requests as req
-
-url1 = 'http://wttr.in/?M2F&lang=ru'
-url2 = 'http://wttr.in/moon?F&lang=ru'
-response = req.get(url1)
-print(response.text)
-response = req.get(url2)
-print(response.text)
