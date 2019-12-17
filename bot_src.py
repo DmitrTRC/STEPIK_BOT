b9a6c2df6590a2c63558e6eed524b936b26e05fb
Dmitry Morozov <9984398@gmail.com>
2019-12-17 13:19:34 +0100

    Class created

--- bot_src.py
+++ bot_src.py
@@ -1,1 +1,22 @@
 import telebot
+import t_token  # Telegram API KEY
+
+
+class MyBot:
+    def __init__(self):
+        self.bot = telebot.TeleBot(t_token.bot_token)
+        self.calls = {}  # User statistics
+        self.GREETING_MSG = '''Приветствую тебя новый пользователь погодного ботика!,
+                     тебе доступны следующие команды:
+                     /start - начало рабоы с ботом
+                     /info - Эта памятка
+                     город,
+                     город дата ( день.месяц ) не дальше 3-х дней,
+                     луна ( moon ),
+                     wiki ( вики ) запрос'''
+
+    @bot.message_handler(func=lambda message: True)
+    def frontier_handler(message):
+        user_id = message.from_user.id
+        if user_id not in calls:
+            calls[user_id] = 0
---
+++ t_token.py
@@ -0,0 +1,1 @@
+bot_token = '1035021917:AAF-3Fki6dWOpQyyTqBbxoFQAEAwYz6fTlk'
