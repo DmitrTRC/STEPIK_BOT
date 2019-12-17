1b81191596f9aee7c3555ddaba29a8f5cabaa0d3
Dmitry Morozov <9984398@gmail.com>
2019-12-17 17:43:55 +0100

    Some changes

--- bot_src.py
+++ bot_src.py
@@ -1,22 +1,28 @@
 import telebot
-import t_token  # Telegram API KEY
+import aux_data  # Telegram API KEY
 
 
-class MyBot:
+class CDispatcher:
+    def __init__(self, table=None):
+        if not table:
+            self.task_manager = {}
+        else:
+            self.task_manager = table
+
+    def is_command(self, command):
+        pass
+
+
+class CWeatherBot:
+    bot = telebot.TeleBot(aux_data.bot_token)
+
     def __init__(self):
-        self.bot = telebot.TeleBot(t_token.bot_token)
         self.calls = {}  # User statistics
-        self.GREETING_MSG = '''Приветствую тебя новый пользователь погодного ботика!,
-                     тебе доступны следующие команды:
-                     /start - начало рабоы с ботом
-                     /info - Эта памятка
-                     город,
-                     город дата ( день.месяц ) не дальше 3-х дней,
-                     луна ( moon ),
-                     wiki ( вики ) запрос'''
+        self.task = CDispatcher()  # Menu task handler
+
 
     @bot.message_handler(func=lambda message: True)
-    def frontier_handler(message):
-        user_id = message.from_user.id
-        if user_id not in calls:
-            calls[user_id] = 0
+    def frontier_handler(self, message):
+        if (user_id := message.from_user.id) not in self.calls:  # Set to 0 User statistics if not found in calls
+            self.calls[user_id] = 0
+            self.bot.send_message(message.chat.id, 'Это бот-погода. Поможет узнать погоду в любом городе. ')
--- t_token.py
+++
@@ -1,1 +0,0 @@
-bot_token = '1035021917:AAF-3Fki6dWOpQyyTqBbxoFQAEAwYz6fTlk'
