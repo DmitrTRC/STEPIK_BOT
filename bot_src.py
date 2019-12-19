1886791a2cc773d5467f7df380a0dd94dc962625
Dmitry Morozov <9984398@gmail.com>
2019-12-19 13:30:11 +0100

    Current weather working

--- bot_src.py
+++ bot_src.py
@@ -1,23 +1,40 @@
 import telebot
+import re
+import datetime
+import requests
+from weatherapi import *
 from Dispatcher import *
 from aux_data import *  # Telegram API KEY, GEO API, , Weather API , extra data
+from datetime import date, timedelta
 
 bot = telebot.TeleBot(bot_token)
+info_weather = CWeatherInfo()
+
 calls = {}  # User statistics
+current_shown_dates = {}  # Selected dates
 
 task = CDispatcher({
 
 })  # Menu task handler@bot.message_handler(func=lambda message: True)
 
 
-@bot.message_handler(func=lambda message: True, commands='start')
+def pares_date(date_str):
+    ret_str = filter(None, re.split("[, /.\-!?:]+", date_str))
+    day, month = map(int, ret_str)
+    return day, month
+
+
+@bot.message_handler(commands=['start'])
 def frontier_handler(message):
-    print(f'Write frontier handling starting... {message.from_user.id=}')
+    print(f'Write frontier handling starting... {message.from_user.id}')
     if message.from_user.id not in calls:  # Set to 0 User statistics if not found in calls
         calls[message.from_user.id] = 0
         bot.send_message(message.chat.id, 'Это бот-погода. Поможет узнать погоду в любом городе. /info - помощь ')
     else:
         calls[message.from_user.id] += 1
+        today = date.today()
+        # cur_date = (today.year, today.month, today.day)
+        current_shown_dates[message.from_user.id] = today
         bot.send_message(message.chat.id, 'Введите город и дату в предела 3 суток.')
 
 
@@ -26,10 +43,58 @@ def info_handler(message):
     bot.send_message(message.from_user.id, GREETING_MSG)
 
 
+@bot.message_handler(commands=['moon'])
+def moon_handler(message):
+    url_moon = 'http://wttr.in/moon'
+    bot.send_message(message.from_user.id,
+                     requests.get(url_moon, params={'QFT': '', 'lang': 'ru'}).text)
+
+
+@bot.message_handler(commands=['weather'])
+def weather(message):
+    report = info_weather.get_detailed_report(info_weather.get_weather(LOCATION.city))
+    base_text = f'Температура на сегодня в городе {LOCATION.city.capitalize()} {round(report[0])} {degree_sign}C;\n'
+    if not report[1]:
+        msg_text = base_text + f'Скорость ветра {report[2]} м/с;\n {report[3]}.'
+    else:
+        msg_text = base_text + f'{report[1]}, {report[2]} м/с;\n {report[3]}.'
+
+    bot.send_message(message.from_user.id, msg_text)
+
+
 @bot.message_handler(commands=['calls'])
 def calls_handle(message):
-    bot.send_message(message.from_user.id, message.from_user.first_name + ' ' + str(calls.get(message.from_user.id)))
+    bot.send_message(message.from_user.id,
+                     message.from_user.first_name + ' ' + str(calls.get(message.from_user.id)))
+
+
+@bot.message_handler(content_types=['text'])
+def weather_parser(message):
+    today = date.today()
+    msg_string = message.text.lower().split()
+    if len(msg_string) == 1:
+        print('weather requests', msg_string[0])
+    elif len(msg_string) == 2:
+        try:
+            day, month = pares_date(msg_string[1])
+        except ValueError:
+            bot.send_message(message.from_user.id, 'Неправильный формат даты ... ')
+        else:
+            try:
+                req_date = date(today.year, month, day)
+            except ValueError:
+                bot.send_message(message.from_user.id, 'Неправильный формат даты ... ')
+            else:
+                dif = req_date - today
+                if 3 < dif.days or dif.days < 0:
+                    bot.send_message(message.from_user.id, 'Неправильный формат даты ( в пределах 3-х суток )')
+                else:
+                    current_shown_dates[message.from_user.id] = req_date  # Selected dates
+                    print('Weather request ', msg_string[0], req_date)
+    else:
+        bot.send_message(message.from_user.id, 'Я тебя не понял !!!')
 
 
 if __name__ == '__main__':
+    # bot.skip_pending = True
     bot.polling(none_stop=True)
---
+++ weatherapi.py
@@ -0,0 +1,78 @@
+import requests
+from aux_data import geo_token, weather_token
+import pyowm
+
+degree_sign = u'\N{DEGREE SIGN}'
+
+
+class CLocationInfo:
+    def __init__(self, auto_init=True):
+        if auto_init:
+            data = self.get_location_info()
+            self.country = data["country_name"]
+            self.city = data["city"]
+            self.zip = data["zip"]
+            self.country_code = data["country_code"]
+        else:
+            self.country = 'Россия'
+            self.city = 'Москва'
+
+    @staticmethod
+    def get_location_info(ip_req=None):
+        return requests.get(
+            'http://api.ipstack.com/check', params={
+                'access_key': geo_token,
+                'format': 1,
+            }).json()
+
+
+class CWeatherInfo:
+    directions = ["Северный ветер",
+                  "Северный, северо-восточный ветер",
+                  "Северо-восточный ветер",
+                  "Восточный, северо-восточный ветер",
+                  "Восточный ветер",
+                  "Восточный, юго-восточный ветер",
+                  "Юго-восточный ветер",
+                  "Южный ветер",
+                  "Южный, юго-западный ветер",
+                  "Юго-западный ветер",
+                  "Западный, юго-западный ветер",
+                  "Западный ветер",
+                  "Западный, северо-западный ветер",
+                  "Северо-западный ветер",
+                  "Северный, северо-западный ветер",
+                  "Северный ветер"
+                  ]
+
+    def __init__(self):
+        self.OWM_TOKEN = weather_token
+        self.owm = pyowm.OWM(self.OWM_TOKEN, language='RU')
+
+    def forecast(self, city, date):
+        obs = self.owm.daily_forecast()
+        weather = obs.get_weather_at(f'{date[1].year}-{date[1].month}-{date[1].day} 12:00:00+00')
+        return weather
+
+    def get_detailed_report(self, weather):
+        temperature = weather.get_temperature('celsius').get('temp')
+        wind_speed = weather.get_wind()['speed']
+        if 'deg' in weather.get_wind():  # для некоторых городов скорость ветра не указана
+            wind_direction = self.directions[int(weather.get_wind()['deg'] / 22.5) - 1]
+        else:
+            wind_direction = None
+
+        weather.get_detailed_status().capitalize()
+        return [temperature, wind_direction, wind_speed, weather.get_detailed_status().capitalize()]
+
+    def get_weather(self, city, days=0):
+        try:
+            obs_point = self.owm.weather_at_place(city)
+            weather = obs_point.get_weather()
+        except:
+            return None
+        else:
+            return weather
+
+
+LOCATION = CLocationInfo(True)
